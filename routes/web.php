fd5dbc30b58b1cc551eb982427a637890ba6a11e
MonirSaikat <monirsaikat1@gmail.com>
2023-10-13 23:32:02 +0600

    added essential routes & controllers

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DepositController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WithdrawlController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::resource('login', AuthController::class)
+    ->only(['index', 'store'])
+    ->middleware('guest');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', DashboardController::class)->name('dashboard');
+Route::resource('users', UserController::class)->only(['create', 'store']);
+Route::resource('deposit', DepositController::class)->only(['index', 'create', 'store']);
+Route::resource('withdrawal', WithdrawlController::class)->only(['index', 'create', 'store']);
