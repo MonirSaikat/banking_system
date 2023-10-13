12ea05b15986d28ff3a4ae724bab7fddf68511ac
MonirSaikat <monirsaikat1@gmail.com>
2023-10-14 00:23:42 +0600

    added middlewares

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,19 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\WithdrawlController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('login', AuthController::class)
-    ->only(['index', 'store'])
-    ->middleware('guest');
 
-Route::get('/', DashboardController::class)->name('dashboard');
-Route::resource('users', UserController::class)->only(['create', 'store']);
-Route::resource('deposit', DepositController::class)->only(['index', 'create', 'store']);
-Route::resource('withdrawal', WithdrawlController::class)->only(['index', 'create', 'store']);
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('login', AuthController::class)
+        ->only(['index', 'store'])
+        ->names([
+            'index' => 'login'
+        ]);
+
+    Route::resource('users', UserController::class)->only(['create', 'store']);
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', DashboardController::class)->name('dashboard');
+    Route::resource('deposit', DepositController::class)->only(['index', 'create', 'store']);
+    Route::resource('withdrawal', WithdrawlController::class)->only(['index', 'create', 'store']);
+});
