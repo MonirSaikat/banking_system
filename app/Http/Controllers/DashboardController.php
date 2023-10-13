e640ec280144c82e99963d35175045f6ca320f03
MonirSaikat <monirsaikat1@gmail.com>
2023-10-14 00:36:41 +0600

    showed transaction list

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function __invoke() {
-        return view('dashboard');
+        $transactions = auth()->user()->transactions;
+
+        return view('dashboard', compact('transactions'));
     }
 }
