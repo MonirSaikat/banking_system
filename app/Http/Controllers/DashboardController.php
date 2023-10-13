d6fba3929e89cc707d9f2a128266c40ee6a559ce
MonirSaikat <monirsaikat1@gmail.com>
2023-10-14 00:40:34 +0600

    added pagination

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function __invoke() {
-        $transactions = auth()->user()->transactions;
+    public function __invoke()
+    {
+        $transactions = auth()
+            ->user()
+            ->transactions()
+            ->paginate(10);
 
         return view('dashboard', compact('transactions'));
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,34 +4,40 @@
 
 @section('content')
 
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">{{ __('Date') }}</th>
-                    <th scope="col">{{ __('Amount') }}</th>
-                    <th scope="col">{{ __('Fee') }}</th>
-                    <th scope="col">{{ __('Type') }}</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($transactions as $transaction)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $transaction->date }}</td>
-                        <td>{{ $transaction->amount }}</td>
-                        <td>{{ $transaction->fee }}</td>
-                        <td>{{ $transaction->transaction_type }}</td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td class="text-center" colspan="5">{{ __('No tranaction to show') }}</td>
-                    </tr>
-                @endforelse
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">{{ __('Date') }}</th>
+                            <th scope="col">{{ __('Amount') }}</th>
+                            <th scope="col">{{ __('Fee') }}</th>
+                            <th scope="col">{{ __('Type') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($transactions as $transaction)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $transaction->date }}</td>
+                                <td>{{ $transaction->amount }}</td>
+                                <td>{{ $transaction->fee }}</td>
+                                <td>{{ $transaction->transaction_type }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td class="text-center" colspan="5">{{ __('No tranaction to show') }}</td>
+                            </tr>
+                        @endforelse
 
-            </tbody>
-        </table>
+                    </tbody>
+                </table>
+            </div>
+
+            {{ $transactions->links() }}
+        </div>
     </div>
 
 @endsection
