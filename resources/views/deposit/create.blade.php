23a7f58122d236b9d52b81bd7bc0f0d3dada356b
MonirSaikat <monirsaikat1@gmail.com>
2023-10-14 00:42:05 +0600

    added date field

--- resources/views/deposit/create.blade.php
+++ resources/views/deposit/create.blade.php
@@ -13,6 +13,17 @@
                     <form action="{{ route('deposit.store') }}" method="POST">
                         @csrf
                         <div class="form-group mb-2">
+                            <label class="form-label" for="date">{{ __('Date') }}</label>
+                            <input type="date" step="any" id="date" class="form-control" name="date"
+                                value="{{ old('date') ?? date('Y-m-d') }}" />
+                            @error('date')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group mb-2">
                             <label class="form-label" for="amount">{{ __('Amount') }}</label>
                             <input type="number" step="any" id="amount" class="form-control" name="amount"
                                 value="{{ old('amount') }}" />
