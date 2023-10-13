8e60c15660a3b37f800ac918e88df19b20de4379
MonirSaikat <monirsaikat1@gmail.com>
2023-10-14 00:14:49 +0600

    added sidebar links

--- resources/views/shared/sidebar.blade.php
+++ resources/views/shared/sidebar.blade.php
@@ -1,16 +1,23 @@
 <nav class="pushy pushy-left">
     <div class="pushy-content">
         <ul>
+            <li class="pushy-link"><a href="{{ route('dashboard') }}">{{ __('Dashboard') }}</a></li>
             <li class="pushy-submenu">
-                <button>Submenu</button>
+                <button>{{ __('Deposit') }}</button>
                 <ul>
-                    <li class="pushy-link"><a href="#">Item 1</a></li>
-                    <li class="pushy-link"><a href="#">Item 2</a></li>
-                    <li class="pushy-link"><a href="#">Item 3</a></li>
+                    <li class="pushy-link"><a href="{{ route('deposit.index') }}">{{ __('Deposit List') }}</a></li>
+                    <li class="pushy-link"><a href="{{ route('deposit.create') }}">{{ __('New Deposit') }}</a></li>
                 </ul>
             </li>
-            <li class="pushy-link"><a href="#">Item 1</a></li>
-            <li class="pushy-link"><a href="#">Item 2</a></li>
+            <li class="pushy-submenu">
+                <button>{{ __('Withdrawal') }}</button>
+                <ul>
+                    <li class="pushy-link"><a href="{{ route('withdrawal.index') }}">{{ __('Withdrawal List') }}</a>
+                    </li>
+                    <li class="pushy-link"><a href="{{ route('withdrawal.create') }}">{{ __('New Withdrawal') }}</a>
+                    </li>
+                </ul>
+            </li>
         </ul>
     </div>
 </nav>
@@ -20,5 +27,5 @@
 
 <!-- Your Content -->
 <div id="container">
-    <button class="menu-btn">&#9776; {{__('Menu')}}</button>
+    <button class="menu-btn">&#9776; {{ __('Menu') }}</button>
 </div>
